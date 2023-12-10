2bbdd9958c83e0a1df56ba3e5c3213fa8d4aed6b
Jacobo Tarrio <jtarrio@gmail.com>
2023-12-10 15:19:26 -0500

    Replace single-file YAML secrets with secrets dir.

--- config/fromflags/fromflags.go
+++ config/fromflags/fromflags.go
@@ -7,14 +7,14 @@ import (
 
 	"jacobo.tarrio.org/jtweb/config"
 	"jacobo.tarrio.org/jtweb/config/yamlconfig"
-	"jacobo.tarrio.org/jtweb/secrets/yamlsecrets"
+	"jacobo.tarrio.org/jtweb/secrets/secretsdir"
 )
 
 var flagConfigFile = flag.String("config_file", "", "The name of the file containing the site's configuration.")
 var flagOutputPath = flag.String("output_path", "", "The full pathname where the rendered HTML files will be output.")
 var flagWebroot = flag.String("webroot", "", "The URI where the generated content will live.")
 var flagPublishUntil = TimeFlag("publish_until", "Publish all posts older than the given date/time.")
-var flagSecretsYaml = flag.String("secrets_yaml", "", "The name of a YAML file containing secrets.")
+var flagSecretsDir = flag.String("secrets_dir", "", "The name of a directory containing secrets files.")
 var flagDryRun = flag.Bool("dry_run", false, "Do not perform the operations.")
 
 func GetConfig() (config.Config, error) {
@@ -27,16 +27,8 @@ func GetConfig() (config.Config, error) {
 	}
 	reader := yamlconfig.NewConfigParser(file)
 
-	if *flagSecretsYaml != "" {
-		file, err := ioutil.ReadFile(*flagSecretsYaml)
-		if err != nil {
-			return nil, err
-		}
-		supplier, err := yamlsecrets.Parse(file)
-		if err != nil {
-			return nil, err
-		}
-		reader.WithSecretSupplier(supplier)
+	if *flagSecretsDir != "" {
+		reader.WithSecretSupplier(secretsdir.Create(*flagSecretsDir))
 	}
 
 	if *flagWebroot != "" {
---
+++ secrets/secretsdir/secretsdir.go
@@ -0,0 +1,27 @@
+package secretsdir
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+)
+
+type secretsDir struct {
+	path string
+}
+
+func Create(path string) *secretsDir {
+	return &secretsDir{path: path}
+}
+
+func (s *secretsDir) GetSecret(key string) (string, error) {
+	if strings.Contains(key, "/") || strings.Contains(key, string(filepath.Separator)) {
+		return "", fmt.Errorf("secret key must not contain a path separator: %s", key)
+	}
+	f, err := ioutil.ReadFile(filepath.Join(s.path, key))
+	if err != nil {
+		return "", err
+	}
+	return string(f), nil
+}
