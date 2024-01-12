883e6ac27c162ba970f4afbf19f1137257fa0e09
Jacobo Tarrio <jtarrio@gmail.com>
2024-01-11 21:08:31 -0500

    Add Dockerfile, remove static content from jtserver.

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# syntax=docker/dockerfile:1
+FROM golang:1.21 AS build-stage
+
+WORKDIR /build
+COPY go.mod go.sum ./
+RUN go mod download
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux go build jacobo.tarrio.org/jtweb/cmd/jtserver
+
+FROM scratch
+WORKDIR /app
+COPY --from=build-stage /build/jtserver ./
+RUN mkdir /data
+VOLUME /data
+EXPOSE 8080
+ENTRYPOINT [ "./jtserver", "--server_address=0.0.0.0:8080" ]
--- cmd/jtserver/jtserver.go
+++ cmd/jtserver/jtserver.go
@@ -16,7 +16,6 @@ import (
 )
 
 var flagServerAddress = flag.String("server_address", "127.0.0.1:8080", "The address where the server will be listening.")
-var flagContentRoot = flag.String("content_root", "./", "The location where the static content resides.")
 
 func getOrigins(cfg config.Config) ([]string, error) {
 	origins := map[string]bool{}
@@ -67,7 +66,6 @@ func main() {
 	mux.Handle("/comments.js", webcontent.ServeCommentsJs())
 	mux.Handle("/admin.html", adminChecker.RequiringAdmin(webcontent.ServeAdminHtml()))
 	mux.Handle("/admin.js", adminChecker.RequiringAdmin(webcontent.ServeAdminJs()))
-	mux.Handle("/", http.FileServer(http.Dir(*flagContentRoot)))
 	server := &http.Server{Addr: *flagServerAddress, Handler: corsChecker.Handler(mux)}
 	log.Printf("Now serving on %s", server.Addr)
 	log.Fatal(server.ListenAndServe())
