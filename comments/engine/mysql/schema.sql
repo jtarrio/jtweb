fae7fb31c7f13b6d2639c9cc5d5ac6239c9991d6
Jacobo Tarrio <jtarrio@gmail.com>
2024-01-11 21:50:36 -0500

    Add schema for MySQL

---
+++ comments/engine/mysql/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `Posts` (
+  `PostId` varchar(255) NOT NULL,
+  `State` tinyint(1) NOT NULL,
+  `StateFromWeb` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`PostId`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+CREATE TABLE `Comments` (
+  `PostId` varchar(255) NOT NULL,
+  `CommentId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Visible` boolean NOT NULL,
+  `Author` varchar(255) NOT NULL,
+  `Date` datetime NOT NULL,
+  `Text` text NOT NULL,
+  PRIMARY KEY (`CommentId`),
+  KEY `PostId` (`PostId`),
+  CONSTRAINT `Comments_ibfk_1` FOREIGN KEY (`PostId`) REFERENCES `Posts` (`PostId`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
